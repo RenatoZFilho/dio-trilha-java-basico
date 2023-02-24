1f61017d36a7276688e35c51af72a2bc9da74b0f
RenatoZFilho <renatozfilho20@hotmail.com>
2023-02-24 00:29:16 -0300

    Incluido anotações sobre Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,36 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        // = | operador de atribuição
+        int idade = 22; // idade recebe valor 22 do tipo inteiro
+
+        // operadores aritmetico:
+        // + -> Soma
+        // - -> Subtração
+        // * -> Multiplicação
+        // / -> Divisão
+
+        int n1 = 5, n2 = 2;
+        String nome = "Renato", sobrenome = " Zanotelli";
+
+        int soma = n1 + n2;
+        String concatenacao = nome + sobrenome;
+        int subtracao = n1 - n2;
+        int multiplicacao = n1 * n2;
+        float divisao = n1 / n2;
+        float modulo = n1 % n2;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Concatenação: " + concatenacao);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+        System.out.println("Resto da divisão: " + modulo);
+
+
+
+
+
+    }
+}
